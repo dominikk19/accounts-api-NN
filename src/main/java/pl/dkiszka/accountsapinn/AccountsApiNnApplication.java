69001a11ac99875f7bc24ed49cad9e15b4399f76
dominikk19 <dominikk19@gmail.com>
2021-06-19 07:18:41 +0200

    initial commit

---
+++ src/main/java/pl/dkiszka/accountsapinn/AccountsApiNnApplication.java
@@ -0,0 +1,13 @@
+package pl.dkiszka.accountsapinn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsApiNnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApiNnApplication.class, args);
+    }
+
+}
